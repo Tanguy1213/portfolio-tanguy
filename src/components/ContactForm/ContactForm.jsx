8a70aed4667e917681c4599fe594e43ccfabb895
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-27 16:12:43 +0200

    another ntlify form test

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -4,19 +4,21 @@ import "./ContactForm.scss";
 function ContactForm() {
   return (
     <div className="contact">
-      <form name="contact" method="post" className="contactForm" data-netlify="true" onSubmit="submit">
-
-        <label for="name">Nom: </label>
-        <input type="text" name="name" />
-
-        <label for="email">Email: </label>
-        <input type="email" name="email" />
-
-        <label for="message">Message: </label>
-        <textarea name="message"></textarea>
-
-        <button type="submit">Envoyer</button>
-      </form>
+      <form name="contact" method="post" className="contactForm">
+      <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>Your Name: <input type="text" name="name"/></label>
+          </p>
+          <p>
+            <label>Your Email: <input type="email" name="email"/></label>
+          </p>
+          <p>
+            <label>Message: <textarea name="message"></textarea></label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
     </div>
   );
 }
