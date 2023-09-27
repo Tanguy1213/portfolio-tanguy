c9c4c3c3199378c05d26f61b40d839ab463f0016
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-27 16:15:38 +0200

    test netlify form 2

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,11 +6,14 @@ function ContactForm() {
     <div className="contact">
       <form name="contact" method="post" className="contactForm">
       <input type="hidden" name="form-name" value="contact" />
-            <label>Your Name: <input type="text" name="name"/></label>
+            <label>Your Name:</label> 
+            <input type="text" name="name"/>
           
-            <label>Your Email: <input type="email" name="email"/></label>
+            <label>Your Email:</label> 
+            <input type="email" name="email"/>
           
-            <label>Message: <textarea name="message"></textarea></label>
+            <label>Message: </label>
+            <textarea name="message"></textarea>
           
             <button type="submit">Send</button>
           
