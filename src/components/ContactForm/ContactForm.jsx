912693b374be2397d68141cf95fa1ea749a1d1aa
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-28 00:39:27 +0200

    annulation test modal

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,62 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ContactForm.scss";
 
 function ContactForm() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  // Fonction pour ouvrir la modale
-  const openModal = () => {
-    setIsModalOpen(true);
-  };
-
-  // Fonction pour fermer la modale
-  const closeModal = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Soumettez le formulaire ici (votre code de traitement).
-
-    // Après avoir soumis le formulaire, ouvrez la modale de confirmation.
-    openModal();
-  };
-
   return (
-    <div>
-      <form
-        name="contact"
-        method="post"
-        className="contactForm"
-        onSubmit={handleSubmit}
-      >
-        <input type="hidden" name="form-name" value="contact"/>
-        <label>Nom :</label>
-        <input type="text" name="name" required={true} />
-        <label>Email :</label>
-        <input type="email" name="email" required={true} />
-        <label>Message :</label>
-        <textarea
-          name="message"
-          required={true}
-          placeholder="Entrez votre message ici !"
-        ></textarea>
-        <button type="submit">Envoyer</button>
-      </form>
-
-      {/* Modale de confirmation */}
-      {isModalOpen && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <h2>Confirmation de soumission</h2>
-            <p>Votre message a été soumis avec succès!</p>
-          </div>
-        </div>
-      )}
-    </div>
+      <form name="contact" method="post" className="contactForm">
+      <input type="hidden" name="form-name" value="contact" />
+      
+            <label>Nom :</label> 
+            <input type="text" name="name" required={true}/>
+          
+            <label>Email :</label> 
+            <input type="email" name="email" required={true}/>
+          
+            <label>Message :</label> 
+            <textarea name="message" required={true} placeholder="Entrez votre message ici !" ></textarea>
+          
+            <button type="submit">Envoyer</button>
+          
+        </form>
   );
 }
 
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,36 +1,12 @@
 import "./Contact.scss";
-import React, { useState } from "react";
 import Particles from "../../components/Particles";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import { NavLink } from "react-router-dom";
 import Loader from "../../components/Loader/Loader";
 
 function Contact() {
-  const [showModal, setShowModal] = useState(false);
-
-  // Fonction pour afficher la modale
-  const openModal = () => {
-    setShowModal(true);
-  };
-
-  // Fonction pour fermer la modale
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
   return (
     <div className="layout-contact">
-      {showModal && (
-        <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={closeModal}>
-              &times;
-            </span>
-            <h2>Thank You!</h2>
-            <p>Your message has been submitted successfully.</p>
-          </div>
-        </div>
-      )}
       <Loader />
       <div className="goBack-link">
         <NavLink to="/">
@@ -38,9 +14,10 @@ function Contact() {
         </NavLink>
       </div>
       <main className="contact-content">
-        <h1 className="form-title">Formulaire de contact</h1>
+      <h1 className="form-title">Formulaire de contact</h1>
         <section className="contact-section">
-          <ContactForm onSuccess={openModal} />
+            
+          <ContactForm />
         </section>
       </main>
       <Particles />
