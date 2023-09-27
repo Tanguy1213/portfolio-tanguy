ea2f7ba63955c7c2445e92ae472a48f2f65dada2
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-27 16:27:43 +0200

    requierd for form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -6,11 +6,15 @@ function ContactForm() {
     <div className="contact">
       <form name="contact" method="post" className="contactForm">
       <input type="hidden" name="form-name" value="contact" />
-            <label>Your Name: <input type="text" name="name"/></label>
+      
+            <label>Your Name:</label> 
+            <input type="text" name="name" required="true"/>
           
-            <label>Your Email: <input type="email" name="email"/></label>
+            <label>Your Email:</label> 
+            <input type="email" name="email" required="true"/>
           
-            <label>Message: <textarea name="message"></textarea></label>
+            <label>Message:</label> 
+            <textarea name="message" required="true"></textarea>
           
             <button type="submit">Send</button>
           
