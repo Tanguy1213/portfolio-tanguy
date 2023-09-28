2e2266278ae003eaada9409d56dadb2d0dd7e03e
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-28 04:30:25 +0200

    setting up clearing form after posting datas

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
-import { NavLink } from "react-router-dom"; // Importez NavLink si vous ne l'avez pas déjà fait
+import { NavLink } from "react-router-dom";
 import "./ContactForm.scss";
 
 function ContactForm() {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -20,10 +25,25 @@ function ContactForm() {
     })
       .then(() => {
         openModal();
+        // Réinitialisez les valeurs des champs après la soumission réussie
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
       })
       .catch((error) => {
         console.error("Erreur lors de la soumission du formulaire :", error);
       });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    // Mettez à jour l'état local formData lorsque les champs changent
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
   return (
@@ -35,30 +55,39 @@ function ContactForm() {
         onSubmit={handleSubmit}
       >
         <input type="hidden" name="form-name" value="contact" />
-
         <label>Nom :</label>
-        <input type="text" name="name" required={true} />
-
+        <input
+          type="text"
+          name="name"
+          required={true}
+          value={formData.name} // Utilisez la valeur de l'état local
+          onChange={handleChange} // Utilisez la fonction de gestion du changement
+        />
         <label>Email :</label>
-        <input type="email" name="email" required={true} />
-
+        <input
+          type="email"
+          name="email"
+          required={true}
+          value={formData.email} // Utilisez la valeur de l'état local
+          onChange={handleChange} // Utilisez la fonction de gestion du changement
+        />
         <label>Message :</label>
         <textarea
           name="message"
           required={true}
-          placeholder="Entrez votre message ici !"
+          placeholder="Entrez votre message ici!"
+          value={formData.message} // Utilisez la valeur de l'état local
+          onChange={handleChange} // Utilisez la fonction de gestion du changement
         ></textarea>
-
         <button type="submit">Envoyer</button>
       </form>
-
       {/* Modal */}
       <Modal
         isOpen={isModalOpen}
         onRequestClose={() => setIsModalOpen(false)}
         contentLabel="Message envoyé"
-        className="custom-modal" // Appliquez une classe CSS personnalisée pour la modal
-        overlayClassName="custom-overlay" // Classe CSS pour le fond de la modal
+        className="custom-modal"
+        overlayClassName="custom-overlay"
       >
         <h2>Message envoyé avec succès!</h2>
         <p>Merci pour votre message.</p>
