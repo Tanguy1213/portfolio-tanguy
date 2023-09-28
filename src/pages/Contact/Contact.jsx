fab629590659e8b3e1f672c44fc482a87efce6f2
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-28 19:45:11 +0200

    ajout d'une animation CSS de courrier

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -12,9 +12,29 @@ function Contact() {
         </NavLink>
       </div>
       <main className="contact-content">
-        <h1 className="form-title">Formulaire de contact</h1>
-        <section className="contact-section">
+        <h2 className="form-title">Formulaire de contact</h2>
+        <section className="contact-section-1">
           <ContactForm />
+        </section>
+        <section className="contact-section-2">
+          <h2 className="mail-title">M'envoyer directement un mail</h2>
+          <a href="mailto:tanguystrub08@gmail.com" className="letter-image">
+            <div className="animated-mail">
+              <div className="back-fold"></div>
+              <div className="letter">
+                <div className="letter-border"></div>
+                <div className="letter-title"></div>
+                <div className="letter-context"></div>
+                <div className="letter-stamp">
+                  <div className="letter-stamp-inner"></div>
+                </div>
+              </div>
+              <div className="top-fold"></div>
+              <div className="body"></div>
+              <div className="left-fold"></div>
+            </div>
+            <div className="shadow"></div>
+          </a>
         </section>
       </main>
       <Particles />
