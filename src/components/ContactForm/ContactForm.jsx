e56689a4a33c5b5e4aa6b8cab66f60bfb0814bd6
Tanguy Strub <tanguystrub08@gmail.com>
2023-09-28 04:14:35 +0200

    Ajout d'une modal pour la confirmation d'envoi

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
+import { NavLink } from "react-router-dom"; // Importez NavLink si vous ne l'avez pas déjà fait
 import "./ContactForm.scss";
 
 function ContactForm() {
-  const handleSubmit = (event) => {
-    event.preventDefault(); // Empêche le comportement par défaut du formulaire
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // Récupérez les données du formulaire
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
     const formData = new FormData(event.target);
 
-    // Utilisez fetch pour soumettre les données à Netlify
     fetch("/", {
       method: "POST",
       body: formData,
     })
       .then(() => {
-        // Redirigez vers la page de confirmation ou effectuez votre transition personnalisée ici
-        window.location.href = "/"; // Remplacez par le chemin de votre page de confirmation
+        openModal();
       })
       .catch((error) => {
         console.error("Erreur lors de la soumission du formulaire :", error);
@@ -23,29 +27,45 @@ function ContactForm() {
   };
 
   return (
-    <form
-      name="contact"
-      method="post"
-      className="contactForm"
-      onSubmit={handleSubmit}
-    >
-      <input type="hidden" name="form-name" value="contact" />
+    <div>
+      <form
+        name="contact"
+        method="post"
+        className="contactForm"
+        onSubmit={handleSubmit}
+      >
+        <input type="hidden" name="form-name" value="contact" />
 
-      <label>Nom :</label>
-      <input type="text" name="name" required={true} />
+        <label>Nom :</label>
+        <input type="text" name="name" required={true} />
 
-      <label>Email :</label>
-      <input type="email" name="email" required={true} />
+        <label>Email :</label>
+        <input type="email" name="email" required={true} />
 
-      <label>Message :</label>
-      <textarea
-        name="message"
-        required={true}
-        placeholder="Entrez votre message ici !"
-      ></textarea>
+        <label>Message :</label>
+        <textarea
+          name="message"
+          required={true}
+          placeholder="Entrez votre message ici !"
+        ></textarea>
 
-      <button type="submit">Envoyer</button>
-    </form>
+        <button type="submit">Envoyer</button>
+      </form>
+
+      {/* Modal */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Message envoyé"
+        className="custom-modal" // Appliquez une classe CSS personnalisée pour la modal
+        overlayClassName="custom-overlay" // Classe CSS pour le fond de la modal
+      >
+        <h2>Message envoyé avec succès!</h2>
+        <p>Merci pour votre message.</p>
+        <NavLink to="/">Retour à la page d'accueil</NavLink>
+        <button onClick={() => setIsModalOpen(false)}>Fermer</button>
+      </Modal>
+    </div>
   );
 }
 
