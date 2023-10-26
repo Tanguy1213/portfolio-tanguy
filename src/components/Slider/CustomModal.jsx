0d95ce670da158fd95a151bf8cfe006ba89eb9d1
Tanguy Strub <tanguystrub08@gmail.com>
2023-10-26 20:32:48 +0200

    ajout des compétences dans la modal de chaque projets

--- src/components/Slider/CustomModal.jsx
+++ src/components/Slider/CustomModal.jsx
@@ -17,6 +17,13 @@ const modalStyle = {
   },
 };
 
+const technologiesIcons = {
+  React: "fa-brands fa-react",
+  HTML: "fa-brands fa-html5",
+  SASS: "fa-brands fa-sass",
+  CSS: "fa-brands fa-css3-alt",
+  Javascript: "fa-brands fa-square-js",
+};
 function CustomModal({ isOpen, closeModal, cardData }) {
   const [modalClass, setModalClass] = useState("modal");
 
@@ -43,33 +50,52 @@ function CustomModal({ isOpen, closeModal, cardData }) {
             <div className="text-wrapper">
               <h3>Projet : {cardData.title}</h3>
               <p>{cardData.description}</p>
-              <p>
-                Compétences : <br />
-                <strong>{cardData.compétences.join(", ")}</strong>
-              </p>
+              <div className="tech-wrapper">
+                <h4>Technologies :</h4>
+                <div>
+                {cardData.technologies.map((technologie, index) => (
+                  <span key={index}>
+                    {technologiesIcons[technologie] ? (
+                      <>
+                        <i className={technologiesIcons[technologie]} />
+                      </>
+                    ) : (
+                      technologie
+                    )}
+                    {index < cardData.technologies.length - 1 && (
+                      <span className="split-style"> | </span>
+                    )}
+                  </span>
+                ))}
+                </div>
+              </div>
             </div>
           </div>
           <div className="link-wrapper">
-          {cardData.githubLink && ( // Vérifiez si githubLink existe
-        <a
-          href={cardData.githubLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="buttonStyle"
-        >
-          Voir sur GitHub
-        </a>
-      )}
-      {cardData.siteLink && ( // Vérifiez si siteLink existe
-        <a
-          href={cardData.siteLink}
-          target="_blank"
-          rel="noopener noreferrer"
-          className="buttonStyle"
-        >
-          Voir le site
-        </a>
-      )}
+            <a
+              href={cardData.githubLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`buttonStyle ${
+                !cardData.githubLink ? "disabled" : ""
+              }`}
+              disabled={!cardData.githubLink}
+            >
+              {cardData.githubLink
+                ? "Voir sur GitHub"
+                : "Pas de lien GitHub disponible"}
+            </a>
+            <a
+              href={cardData.siteLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`buttonStyle ${!cardData.siteLink ? "disabled" : ""}`}
+              disabled={!cardData.siteLink}
+            >
+              {cardData.siteLink
+                ? "Voir le site"
+                : "Pas de lien vers le site disponible"}
+            </a>
           </div>
           <button onClick={closeModal}>X</button>
         </div>
