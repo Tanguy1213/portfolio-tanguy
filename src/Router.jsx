b39a7f3d053cd9b29dde3721c6b07bbe36138bf4
Tanguy Strub <tanguystrub08@gmail.com>
2024-01-31 00:16:30 +0100

    test build netlify

--- src/Router.jsx
+++ src/Router.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Pages
 import Home from "./pages/Home/Home";
 import Contact from "./pages/Contact/Contact";
-import Sitemap from "../public/sitemap.xml";
+import Sitemap from "./Sitemap";
 
 const Router = () => {
   return (
